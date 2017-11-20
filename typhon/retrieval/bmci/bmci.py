d1b9ba4038363be91856e87c0ca93dd8536d7cee
simonpf <simonpf@chalmers.se>
2017-11-20 10:04:52 +0100

    Testing and documentation.

--- typhon/retrieval/bmci/bmci.py
+++ typhon/retrieval/bmci/bmci.py
@@ -15,13 +15,18 @@ class BMCI:
 
     This class implements methods for solving Bayesian inverse problems using
     Monte Carlo integration. The method uses a data base of atmospheric states
-    :math:`x_i` and corresponding  measurements :math:`\mathbf{y}_i`, which are used
-    to compute integrals of the posterior distribution by means of importance sampling.
+    :math:`x_i` and corresponding  observations :math:`\mathbf{y}_i`, which are used
+    to compute integrals of the posterior distribution by means of importance sampling:
+
+    .. math::
+
+        \int_a^b f(x') p(x' | \mathbf{y}) \: dx' \approx
+        \sum_{a \leq x_i \leq b} \frac{w_i(\mathbf{y}) f(x)}{\sum_j w_j(\mathbf{y})}
 
     The measurements in the database are assumed to be given by a
-    :math:`n \\times m` matrix :math:`\mathbf{y}`, where n is the number of
+    :math:`n \times m` matrix :math:`\mathbf{y}`, where n is the number of
     cases in the database. Currently only scalar retrievals are supported,
-    which means that :math:`\mathbf{x}` is assumed to be :math:`n`-element vector
+    which means that :math:`\mathbf{x}` is assumed to an be :math:`n`-element vector
     containing the retrieval quantities corresponding to the observations in
     :math:`\mathbf{y}`.
 
@@ -38,34 +43,34 @@ class BMCI:
 
     Attributes
 
-        x: 1D array
+        x: numpy.array, shape = (n,)
            The retrieval quantity corresponding to the atmospheric states represented
            in the data base.
 
-        y: 2D array
+        y: numpy.array, shape = (n, m)
            The measured or simulated brightness temperatures corresponding to the
            atmospheric states represented in the data base.
 
-        s_o: 2D array
+        s_o: numpy.array, shape = (m, m)
            The covariance matrix describing the measurement uncertainty.
 
         n: int
            The number of entries in the retrieval database.
 
         m: int
-           The number of channels in a single measurement :math:`\mathbf{y}_i`
+           The number of channels in a single measurement :math:`\mathbf{y}_i`.
 
-        pc1: 1D array
+        pc1: numpy.array, shape = (m, )
             The eigenvector corresponding to the smallest eigenvalue of the
             observation uncertainty covariance matrix. Along this vector the
             entries in the database will be ordered, which can be used to
             accelerate the retrieval.
 
-        pc1_e: float64
+        pc1_e: float
             The smallest eigenvalue of the observation uncertainty covariance
             matrix.
 
-        pc1_proj: 1D array with n elements
+        pc1_proj: numpy.array, shape (n, )
             The projections of the measurements in `y` onto `pc1` along which
             the database entries are ordered.
 
@@ -78,11 +83,12 @@ class BMCI:
 
         Args
 
-            y: 2D array
-            The measured or simulated brightness temperatures corresponding
-            to the atmospheric states represented in the data base. These
-            are sorted along the eigenvector of the observation uncertainty
-            covariance matrix with the smallest eigentvector.
+            y(numpy.array): 2D array containing the measured or simulated
+                            brightness temperatures corresponding to the
+                            atmospheric states represented in the data base.
+                            These are sorted along the eigenvector of the
+                            observation uncertainty covariance matrix with
+                            the smallest eigentvector.
 
             x: 1D array
             The retrieval quantity corresponding to the atmospheric states
@@ -112,13 +118,10 @@ class BMCI:
 
         w, v = np.linalg.eig(self.s_o)
 
-        # Columns of u are eigenvectors y^T * y (Covariance Matrix)
-        u, s, _ = np.linalg.svd(np.transpose(y - self.y_mean) / np.sqrt(self.n),
-                                full_matrices = False)
-
         inds = np.argsort(w)
-        self.pc1_e = w[inds[0]] #s[0] ** 2.0
-        self.pc1 = v[:, inds[0]] #u[:, 0]
+        print(w[inds])
+        self.pc1_e = 1.0 / w[inds[0]]
+        self.pc1 = v[:, inds[0]]
         self.pc1_proj = np.dot((y - self.y_mean), self.pc1)
 
         indices = np.argsort(self.pc1_proj)
@@ -126,24 +129,50 @@ class BMCI:
         self.x = x[indices]
         self.y = y[indices, :]
 
-    def __find_hits(self, y_obs, ds = 10.0):
-        """
+        self.x_sorted_inds = np.argsort(self.x)
+
+
+    def __find_hits(self, y_obs, x2_max = 10.0):
+        r"""
         Finds the range of indices in the database guaranteed to have a greater
-        :math:`\chi^2` value than `ds`.
+        :math:`\chi^2` value than `x2_max`.
 
         Args:
 
             y_obs: 1D array
-                   The measurement for which to compute the 
+                   The measurement for which to compute the range of elements
+                   to include in the weight calculation.
+
+        Returns: Tuple of integers `(i_l, i_u, n_hits)`
+
+            i_l: int
+                 The lower index of the range of elements to include in the
+                 weight calculation.
+
+            i_u: int
+                 The upper index of the range of elements to include in the
+                 weight calculation.
+
+            n_hits: int
+                 The number of hits in the that are within the specified
+                 :math:`\chi^2` limits.
+
         """
-        y_proj = np.dot(self.pc1, (y_obs - self.y_mean))
-        s_l = y_proj - np.sqrt(ds / self.pc1_e)
-        s_u = y_proj + np.sqrt(ds / self.pc1_e)
+        y_proj = np.dot(self.pc1, (y_obs - self.y_mean).ravel())
+        s_l = y_proj - np.sqrt(2.0 * x2_max / self.pc1_e)
+        s_u = y_proj + np.sqrt(2.0 * x2_max / self.pc1_e)
         inds = np.searchsorted(self.pc1_proj, np.array([s_l, s_u]))
 
         return inds[0], inds[1], inds[1] - inds[0]
 
-    def weights(self, y, y_train = None):
+    def __gauss_prob(self, y_obs, y_database):
+
+        dy = y_database - y_obs.reshape(1, -1)
+        ws = dy * np.dot(dy, self.s_o_inv)
+        ws = np.exp(-0.5 * ws.sum(axis=1, keepdims=True))
+        return ws
+
+    def weights(self, y_obs, x2_max = -1.0):
         r"""
         Compute the importance sampling weights for a given observation `y`.
         If `y_train` is given it will be used as the database observations
@@ -172,17 +201,17 @@ class BMCI:
 
         Return
         """
-        if not y_train:
-            y_train = self.y
-
-        y = y.reshape((1,-1))
-        dy = y_train - y
-        ws = dy * np.dot(dy, self.s_o_inv)
-        ws = np.exp(-0.5 * ws.sum(axis=1, keepdims=True))
-        return ws
+        if x2_max < 0.0:
+            ws = self.__gauss_prob(y_obs, self.y )
+            c  = ws.sum()
+            return 0, self.n, ws
+        else:
+            i_l, i_u, n_hits = self.__find_hits(y_obs, x2_max)
+            ws = self.__gauss_prob(y_obs, self.y[i_l : i_u, :])
+            return i_l, i_u, ws
 
     def predict(self, y_obs, x2_max = -1.0):
-        """
+        r"""
         This performs the BMCI integration to approximate the mean and variance
         of the posterior distribution:
 
@@ -196,53 +225,80 @@ class BMCI:
 
         If the keyword arg `x2_max` is provided, then the weights will be
         computed exluding database entries that are guaranteed to have a
-        :math:`\Chi^2` value higher than `x2_max`.
+        :math:`\chi^2` value higher than `x2_max`.
 
-        Args
+        Arguments:
 
-            y_obs: 2D array
-                   The observations for which to perform the retrieval.
+            y_obs (numpy.ndarray): 2-D array containing the observations for
+                                   which to perform the retrieval.
 
-        Returns
+        Returns:
 
-            `xs`: 1D array
-                  The retrieved posterior mean of the retrieval quantity
-                  x.
-            `vs': 1D array
-                  The retrieved posterior standard deviations.
+            A tuple `(xs, sigmas)` containing the retrieved means (`xs`) and
+            the corresponding standard deviations (`sigmas`).
 
         """
-        if x2_max < 0.0:
-            xs = np.zeros(y_obs.shape[0])
-            vs = np.zeros(y_obs.shape[0])
-            for i in range(y_obs.shape[0]):
-                ws = self.weights(self.y, y_obs[i,:])
-                c  = ws.sum()
-                print(c.shape)
-                print(ws.shape)
-                print(self.x.shape)
-                if c > 0.0:
-                    xs[i] = np.sum(self.x * ws.ravel() / c)
-                    vs[i] = np.sqrt(np.sum(self.x ** 2.0 * ws.ravel() / c))
-                else:
-                    xs[i] = np.mean(self.x)
-                    vs[i] = np.std(self.x)
-                if i % 10 == 0:
-                    print("progress: " + str(i))
-            return xs, vs
-        else:
-            xs = np.zeros(y_obs.shape[0])
-            for i in range(y_obs.shape[0]):
-                i_l, i_u, n_hits = self.__find_hits(y_obs[i, :], x2_max)
-                if n_hits > 0:
-                    ws = self.weights(self.y[i_l : i_u, :], y_obs[i,:])
-                    c  = ws.sum()
-                    xs[i] = np.sum(self.x[i_l : i_u] * ws.ravel() / c)
-                    vs[i] = np.sqrt(np.sum(self.x[i_l : i_u]**2 * ws.ravel() / c))
-                else:
-                    xs[i] = np.float('nan')
-                    vs[i] = np.float('nan')
-            return xs, vs
+        xs = np.zeros(y_obs.shape[0])
+        sigmas = np.zeros(y_obs.shape[0])
+
+        for i in range(y_obs.shape[0]):
+            i_l, i_u, ws = self.weights(y_obs[i, :], x2_max)
+            c = ws.sum()
+            if c > 0.0:
+                xs[i] = np.sum(self.x[i_l:i_u].ravel() * ws.ravel() / c)
+                sigmas[i] = np.sqrt(np.sum(
+                    (self.x[i_l:i_u].ravel() - xs[i]) ** 2.0 * ws.ravel() / c))
+            else:
+                xs[i] = np.float("nan")
+                sigmas[i] = np.float("nan")
+        return xs, sigmas
+
+    def crps(self, y_obs, x_true, x2_max = -1.0):
+        r"""
+
+        This function approximates the cumulative probability density of the
+        posterior distribution for the given observations in `y_obs` and
+        computes the continuously ranked probability score:
+
+        .. math::
+            CRPS(\mathbf{y}, x) = \int_-\infty^\infty (F_{x | \mathbf{y}}(x')
+            - \mathrm{1}_{x < x'})^2 \: dx'
+
+        Arguments:
+
+            y_obs(numpy.ndarray): 2-D array with shape `(n, m)` containing the
+                                   n observations for which to evaluate the CRPS.
+            x_true(numpy.ndarray): 1-D array containing the `n` x values to test
+                                   the predictions against.
+
+        """
+        n = y_obs.shape[0]
+        scores = np.zeros(n)
+
+        for i in range(n):
+            i_l, i_u, ws = self.weights(y_obs[i, :], x2_max)
+
+            inds = np.where((i_l <= self.x_sorted_inds) * (self.x_sorted_inds < i_u))
+            print(inds)
+            inds = self.x_sorted_inds[inds] - i_l
+
+            ws = ws[inds]
+            xs = self.x[i_l:i_u][inds]
+
+            indicator = np.zeros(i_u - i_l)
+            indicator[xs > x_true[i]] = 1.0
+
+            ws_cum = ws.cumsum()
+
+            if ws_cum[-1] > 0.0:
+                ws_cum /= ws_cum[-1]
+                scores[i] = np.trapz((ws_cum - indicator) ** 2.0,
+                                     xs)
+            else:
+                scores[i] = np.float("nan")
+
+        return scores
+
 
     def predict_quantiles(self, y_obs, taus):
         r"""
@@ -254,7 +310,7 @@ class BMCI:
             F(x) & = \int_{-\infty}^{x} p(x') \: dx'
             \sim \sum_{x_i < x} w_i
 
-        and then interpolating :math:`F^{-1}` to the requested quantiles.
+        and then interpolating :math:`F^{-1}` to obtain the requested quantiles.
 
         Args:
 
@@ -273,8 +329,11 @@ class BMCI:
         """
         xs = np.zeros((y_obs.shape[0], taus.size))
         for i in range(y_obs.shape[0]):
-            ws = self.weights(y_obs[i,:])
-            ws_sum = np.cumsum(ws)
-            ws_sum /= ws_sum[-1]
-            xs[i, :] = np.interp(taus, ws_sum, self.x.ravel())
+            i_l, i_u, ws = self.weights(y_obs[i,:])
+            ws_sum = np.cumsum(ws[self.x_sorted_inds[i_l, i_u]])
+            if ws_sum > 0.0:
+                ws_sum /= ws_sum[-1]
+                xs[i, :] = np.interp(taus, ws_sum, self.x.ravel())
+            else:
+                xs[i, :] = np.float("nan")
         return xs
